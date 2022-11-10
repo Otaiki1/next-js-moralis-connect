5288391f4f39932774e75289f9ab510c3382a4eb
Otaiki1 <otaikisadiq2804@gmail.com>
2022-11-10 12:13:21 +0100

    create , link connectButton

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@ import {ConnectButton} from "web3uikit";
 export default function Header(){
     return(
         <nav>
+            Head
             <ConnectButton moralisAuth={false} />   
         </nav>
        
